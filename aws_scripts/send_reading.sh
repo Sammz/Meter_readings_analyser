57b0c44abb2bf6d7fcc36d97b6547e5d72efe60d
Sam <Sammz@users.noreply.github.com>
2025-06-05 17:43:18 +0100

    add one liner for curl command commented out, improve response printing

--- aws_scripts/send_reading.sh
+++ aws_scripts/send_reading.sh
@@ -15,8 +15,11 @@ DATE=$3
 LAMBDA_URL="https://rk7tfl27zpfw7rfpfyujfiby4e0cners.lambda-url.eu-west-2.on.aws/"
 
 # Send the request
-curl -w '%{http_code}\n' -X POST $LAMBDA_URL \
+curl -w '\nResponse code: %{http_code}\n' -X POST $LAMBDA_URL \
   -H "Content-Type: application/json" \
   -d "{\"peak\": $PEAK, \"off_peak\": $OFF_PEAK, \"day\": \"$DATE\", \"api_key\": \"$METER_READINGS_PROCESSOR_LAMBDA_API_KEY\"}"
 
 echo "woohoo"
+
+
+#curl -w '\nResponse code: %{http_code}\n' -X POST https://rk7tfl27zpfw7rfpfyujfiby4e0cners.lambda-url.eu-west-2.on.aws/ -H "Content-Type: application/json" -d "{\"peak\": 1, \"off_peak\": 1, \"day\": \"2020-10-10\", \"api_key\": \"hoi\"}"
