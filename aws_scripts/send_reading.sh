9a9d9ba78886f0abf826526582a6ded1bc6c8f62
Sam <Sammz@users.noreply.github.com>
2025-06-05 11:46:42 +0100

    add send_reading.sh for sending readings from termux

---
+++ aws_scripts/send_reading.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if all arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./send_reading.sh <peak> <off_peak> <date>"
+    echo "Example: ./send_reading.sh 14 33 2025-06-03"
+    exit 1
+fi
+
+PEAK=$1
+OFF_PEAK=$2
+DATE=$3
+
+# Your Lambda function URL
+LAMBDA_URL="https://rk7tfl27zpfw7rfpfyujfiby4e0cners.lambda-url.eu-west-2.on.aws/"
+
+# Send the request
+curl -w '%{http_code}\n' -X POST $LAMBDA_URL \
+  -H "Content-Type: application/json" \
+  -d "{\"peak\": $PEAK, \"off_peak\": $OFF_PEAK, \"day\": \"$DATE\", \"api_key\": \"$METER_READINGS_PROCESSOR_LAMBDA_API_KEY\"}"
+
+echo "woohoo"
