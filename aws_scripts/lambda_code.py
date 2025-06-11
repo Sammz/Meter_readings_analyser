bbecb3d8ca534a93b9fe524a310cc0796b39e54b
Sam <Sammz@users.noreply.github.com>
2025-06-11 23:42:18 +0100

    Add validity checks for date and readings

--- aws_scripts/lambda_code.py
+++ aws_scripts/lambda_code.py
@@ -1,5 +1,5 @@
 import boto3
-import datetime
+from datetime import datetime, timedelta
 from boto3.dynamodb.conditions import Key
 import os
 import traceback
@@ -24,41 +24,53 @@ def lambda_handler(event, context):
 
         current_peak = body['peak']
         current_off_peak = body['off_peak']
-        current_date = body['date']  # Format: YYYY-MM-DD
+        current_date_string = body['date']  # Format: YYYY-MM-DD
+        current_date = ""
+
+        # Check validity of the date
+        try:
+            current_date = datetime.strptime(current_date_string, "%Y-%m-%d").date()
+        except ValueError:
+            current_date_string = "wrong"
 
         # Validate input
-        if not current_date or not isinstance(current_peak, int) or not isinstance(current_off_peak, int):
+        if current_date_string == "wrong" or not isinstance(current_peak, int) or not isinstance(current_off_peak, int):
             return {
                 'statusCode': 400,
                 'body': 'Invalid input. Required: peak (integer), off_peak (integer), date (YYYY-MM-DD)'
             }
 
-        # Get tariff information
-        response = tariff_table.query(
-            KeyConditionExpression=Key('pk').eq('tariff') & Key('start_date').lte(current_date),
-            ScanIndexForward=False,  # get latest first
-            Limit=1
-        )
-
-        tariff = response['Items'][0]
-        peak_rate = tariff.get('peak_rate')
-        off_peak_rate = tariff.get('off_peak_rate')
-        standing_charge = tariff.get('standing_charge')
-
         # Get previous reading
         response = meter_table.query(
-            KeyConditionExpression=Key('pk').eq('reading') & Key('date').lt(current_date),
+            KeyConditionExpression=Key('pk').eq('reading') & Key('date').lte(current_date_string),
             ScanIndexForward=False,  # get latest first
             Limit=1
         )
+        # Check if we got a previous reading for the supplied date
+        if not len(response['Items']) == 1:
+            return {
+                'statusCode': 400,
+                'body': 'No previous reading found for the supplied date'
+            }
 
         previous_reading = response['Items'][0]
         previous_peak = previous_reading.get('peak')
         previous_off_peak = previous_reading.get('off_peak')
-        previous_date = datetime.datetime.strptime(previous_reading.get('date'), '%Y-%m-%d').date()
+        previous_date = datetime.strptime(previous_reading.get('date'), '%Y-%m-%d').date()
 
-        # Transform datestring to date format for calculations, has to be after querying the dynamodb tables
-        current_date = datetime.datetime.strptime(current_date, '%Y-%m-%d').date()
+        # Check if the date has not been recorded yet
+        if current_date == previous_date:
+            return {
+                'statusCode': 400,
+                'body': f'A reading on {current_date_string} has already been stored'
+            }
+
+        # Check if the readings make sense
+        if current_peak < previous_peak or current_off_peak < previous_off_peak:
+            return {
+                'statusCode': 400,
+                'body': 'One of both of the meter values are lower then the previous reading'
+            }
 
         # Calculate the difference between current and previous readings
         delta_peak_usage = current_peak - previous_peak
@@ -66,6 +78,17 @@ def lambda_handler(event, context):
 
         # Calculate the number of days between readings
         days_diff = (current_date - previous_date).days
+
+        # Get tariff information
+        response = tariff_table.query(
+            KeyConditionExpression=Key('pk').eq('tariff') & Key('start_date').lte(current_date_string),
+            ScanIndexForward=False,  # get latest first
+            Limit=1
+        )
+        tariff = response['Items'][0]
+        peak_rate = tariff.get('peak_rate')
+        off_peak_rate = tariff.get('off_peak_rate')
+        standing_charge = tariff.get('standing_charge')
 
         # If there are missing days, create entries with averaged values
         if days_diff > 1:
@@ -77,7 +100,7 @@ def lambda_handler(event, context):
 
             # Create estimated entries for missing days and the current day
             for i in range(1, days_diff + 1):
-                missing_date = previous_date + datetime.timedelta(days=i)
+                missing_date = previous_date + timedelta(days=i)
                 missing_day = missing_date.strftime('%Y-%m-%d')
 
                 if i == days_diff:
@@ -106,13 +129,11 @@ def lambda_handler(event, context):
             # Calculate total usage and cost for the current day
             daily_cost = round((delta_peak_usage * peak_rate) + (delta_off_peak_usage * off_peak_rate) + standing_charge, 3)
 
-            current_date = current_date.strftime('%Y-%m-%d')
-
             # Store the current reading with calculated usage
             meter_table.put_item(
                 Item={
                     'pk': 'reading',
-                    'date': current_date,
+                    'date': current_date_string,
                     'peak': current_peak,
                     'off_peak': current_off_peak,
                     'peak_usage': delta_peak_usage,
