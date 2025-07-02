ee88c2d09740a3ad78ce01b816858b87dbda1f1d
Sam <Sammz@users.noreply.github.com>
2025-07-02 13:42:35 +0100

    change input argument order, print response in a better way

--- aws_scripts/send_reading.sh
+++ aws_scripts/send_reading.sh
@@ -2,24 +2,21 @@
 
 # Check if all arguments are provided
 if [ "$#" -ne 3 ]; then
-    echo "Usage: ./send_reading.sh <peak> <off_peak> <date>"
-    echo "Example: ./send_reading.sh 14 33 2025-06-03"
+    echo "Usage: ./send_reading.sh <date> <peak> <off_peak> "
+    echo "Example: ./send_reading.sh 2025-06-03 14 33 "
     exit 1
 fi
 
-PEAK=$1
-OFF_PEAK=$2
-DATE=$3
-
-# Your Lambda function URL
-LAMBDA_URL="https://rk7tfl27zpfw7rfpfyujfiby4e0cners.lambda-url.eu-west-2.on.aws/"
+DATE=$1
+PEAK=$2
+OFF_PEAK=$3
 
 # Send the request
-curl -w '\nResponse code: %{http_code}\n' -X POST $LAMBDA_URL \
+response=$(curl -w '\nResponse code: %{http_code}\n' -X POST $LAMBDA_URL \
   -H "Content-Type: application/json" \
-  -d "{\"peak\": $PEAK, \"off_peak\": $OFF_PEAK, \"day\": \"$DATE\", \"api_key\": \"$METER_READINGS_PROCESSOR_LAMBDA_API_KEY\"}"
+  -d "{\"peak\": $PEAK, \"off_peak\": $OFF_PEAK, \"date\": \"$DATE\", \"api_key\": \"$METER_READINGS_PROCESSOR_LAMBDA_API_KEY\"}")
 
-echo "woohoo"
+echo "$response" | jq -r 'to_entries[] | "\(.key): \(.value)"'
 
 
-#curl -w '\nResponse code: %{http_code}\n' -X POST https://rk7tfl27zpfw7rfpfyujfiby4e0cners.lambda-url.eu-west-2.on.aws/ -H "Content-Type: application/json" -d "{\"peak\": 1, \"off_peak\": 1, \"day\": \"2020-10-10\", \"api_key\": \"hoi\"}"
+#curl -w '\nResponse code: %{http_code}\n' -X POST $LAMBDA_URL -H "Content-Type: application/json" -d "{\"peak\": 1, \"off_peak\": 1, \"date\": \"2020-10-10\", \"api_key\": \"hoi\"}"
